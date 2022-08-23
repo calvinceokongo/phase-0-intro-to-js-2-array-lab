6f14c1ed4514742f82b6a083d6a5182d2797b2a0
calvince <calokongo@gmail.com>
2022-08-24 00:59:27 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+  function appendCats(name) {
+     var newArray = cats.slice()
+  }
+
+  const cat = ["Milo", "Otis", "Garfield"];
+  cats.pop();{
+    return "Millo", "Otis", "Girlfriend"
+  }
